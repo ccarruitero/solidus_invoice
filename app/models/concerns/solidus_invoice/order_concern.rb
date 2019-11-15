aa57a6ab68d1e7d3ce164e8e6c59d73d735576d7
César Carruitero <ccarruitero@protonmail.com>
2019-11-15 15:42:09 -0500

    basic order concern

---
+++ app/models/concerns/solidus_invoice/order_concern.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module SolidusInvoice
+  module OrderConcern
+    included do
+      has_many :invoices
+      after_save :generate_invoice
+
+      private
+
+      def generate_invoice
+        return unless can_generate_invoice?
+
+        doc_type = bill_address.uid ? '01' : '03'
+        doc_number = next_correlative(doc_type)
+        # generate Spree::Invoice
+        invoices.create(doc_type: doc_type,
+                        doc_number: doc_number,
+                        order: self)
+        # generate Sunat::Invoice
+        invoice = SunatInvoice::Invoice.new(invoice.sunat_attributes)
+        SolidusInvoice.SunatWorker.perform(doc_type, invoice.id)
+      end
+
+      def can_generate_invoice?
+        completed?
+      end
+
+      def next_correlative(doc_type, store_id)
+        store = Spree::Store.where(store_id: store_id)
+        serial = store.invoice_serials.includes(:invoices).find(doc_type: doc_type)
+        number = serial.invoices.order(correlative: :desc).first.correlative + 1
+        "#{serial}-#{number}"
+      end
+    end
+  end
+end
