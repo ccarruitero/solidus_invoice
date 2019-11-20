60bddfec32f6a347b77ce77b83e90e0c9f6e9b70
César Carruitero <ccarruitero@protonmail.com>
2019-11-19 23:09:27 -0500

    remove unused field

--- db/migrate/20191116164034_add_fields_to_spree_invoice.rb
+++ db/migrate/20191116164034_add_fields_to_spree_invoice.rb
@@ -3,6 +3,5 @@
 class AddFieldsToSpreeInvoice < ActiveRecord::Migration[6.0]
   def change
     add_column :spree_invoices, :communicated, :boolean, default: false
-    add_column :spree_invoices, :correlative, :integer
   end
 end
