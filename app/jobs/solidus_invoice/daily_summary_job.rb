b8903a0b79f3cb21e0b810e6f22ca0186098d995
César Carruitero <ccarruitero@protonmail.com>
2019-12-03 19:51:20 -0500

    add daily summary job

--- app/jobs/solidus_invoice/daily_summary_job.rb
+++ app/jobs/solidus_invoice/daily_summary_job.rb
@@ -4,6 +4,29 @@ class SolidusInvoice::DailySummaryJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    # find all doc_type doc without comunication
+    serials = Spree::InvoiceSerial.includes(:invoices).where(
+      doc_type: args[:doc_type],
+      store_id: args[:store_id]
+    )
+
+    lines = []
+
+    serials.each do
+      invoices = serials.invoices.where(communicated: false).order(doc_number: :asc)
+      line = SunatInvoice::SummaryLine.new(document_type: serial.doc_type,
+                                           document_serial: serial.serial,
+                                           start_document_number: invoice.first.doc_number,
+                                           end_document_number: invoice.last.doc_number,
+                                           total_amount: invoices.sum(:total),
+                                           taxable: invoice.sum(:taxable))
+      lines << line
+    end
+
+    # send to sunat
+    client = SunatInvoice::InvoiceClient.new
+    summary = SunatInvoice::DailySummary.new(reference_date: Time.with_zone.yesterday,
+                                             lines: lines)
+    client.dispatch(summary)
   end
 end
--- spec/jobs/solidus_invoice/daily_summary_job_spec.rb
+++ spec/jobs/solidus_invoice/daily_summary_job_spec.rb
@@ -3,5 +3,22 @@
 require 'spec_helper'
 
 RSpec.describe SolidusInvoice::DailySummaryJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:invoice) { factory(:invoice) }
+  let(:subject) {
+    described_class.perform_later(
+      doc_type: invoice.invoice_serial.serial,
+      store_id: invoice.order.store.id
+    )
+  }
+
+  it 'generate summary lines by each invoice' do
+  end
+
+  it 'generate correct daily summary document' do
+  end
+
+  describe '#send_sunat' do
+    it 'call summary client' do
+    end
+  end
 end
