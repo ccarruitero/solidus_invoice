154822463e73d96125229b7b2a3abcd4de69c483
César Carruitero <ccarruitero@protonmail.com>
2019-12-04 00:04:02 -0500

    add some factories

--- lib/solidus_invoice/factories.rb
+++ lib/solidus_invoice/factories.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  # Define your Spree extensions Factories within this file to enable applications, and other extensions to use and override them.
-  #
-  # Example adding this to your spec_helper will load these Factories for use:
-  # require '<%= name %>/factories'
+  factory :invoice, class: 'Spree::Invoice' do
+    doc_number { rand(450) }
+    order { order_ready_to_ship }
+    invoice_serial
+  end
+
+  factory :invoice_serial, class: 'Spree::Invoice' do
+    store
+    doc_type { '03' }
+    serial { 'B023' }
+  end
 end
