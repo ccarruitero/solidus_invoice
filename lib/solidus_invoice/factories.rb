bd6288ebd1a2d3d1274972259c9ecd5646289e80
César Carruitero <ccarruitero@protonmail.com>
2019-12-05 18:19:16 -0500

    fix typos in factories

--- lib/solidus_invoice/factories.rb
+++ lib/solidus_invoice/factories.rb
@@ -3,11 +3,11 @@
 FactoryBot.define do
   factory :invoice, class: 'Spree::Invoice' do
     doc_number { rand(450) }
-    order { order_ready_to_ship }
+    order { create(:order_ready_to_ship) }
     invoice_serial
   end
 
-  factory :invoice_serial, class: 'Spree::Invoice' do
+  factory :invoice_serial, class: 'Spree::InvoiceSerial' do
     store
     doc_type { '03' }
     serial { 'B023' }
