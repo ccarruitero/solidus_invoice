a4fa6171917756f920c024a140b3070675e62294
César Carruitero <ccarruitero@protonmail.com>
2019-12-23 16:10:15 -0500

    fix order generate invoice

--- app/models/concerns/solidus_invoice/order_concern.rb
+++ app/models/concerns/solidus_invoice/order_concern.rb
@@ -14,10 +14,10 @@ module SolidusInvoice
         return unless can_generate_invoice?
 
         doc_type = bill_address.tax_uid ? '01' : '03'
-        doc_number = next_correlative(doc_type)
+        doc_number = next_correlative(doc_type, store.id)
 
         # generate Spree::Invoice
-        invoices.create(doc_type: doc_type,
+        invoices.create(invoice_serial: get_serial(doc_type, store),
                         doc_number: doc_number,
                         order: self)
       end
@@ -26,10 +26,14 @@ module SolidusInvoice
         completed? && paid?
       end
 
+      def get_serial(doc_type, store = Spree::Store.default)
+        store.invoice_serials.find_by(doc_type: doc_type)
+      end
+
       # given a store and document type get next posible correlative
       def next_correlative(doc_type, store_id = Spree::Store.default.id)
         store = Spree::Store.includes(invoice_serials: :invoices).find(store_id)
-        serial = store.invoice_serials.find_by(doc_type: doc_type)
+        serial = get_serial(doc_type, store)
         raise SolidusInvoice::InvalidSerialError unless serial
 
         current = serial&.invoices&.order(doc_number: :desc)&.first&.doc_number || 0
