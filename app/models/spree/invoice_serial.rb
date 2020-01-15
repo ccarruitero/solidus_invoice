0e2bf3e289cd3881082770b098788e51e00eca63
César Carruitero <ccarruitero@protonmail.com>
2020-01-15 14:48:31 -0500

    validates serial scoped by store for invoice serial

--- app/models/spree/invoice_serial.rb
+++ app/models/spree/invoice_serial.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Spree::InvoiceSerial < Spree::Base
-  validates :serial, presence: true
+  validates :serial, presence: true, uniqueness: { scope: :store_id }
   validates :store_id, presence: true
 
   belongs_to :store
--- spec/models/spree/invoice_serial_spec.rb
+++ spec/models/spree/invoice_serial_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Spree::InvoiceSerial, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:serial) }
     it { should validate_presence_of(:store_id) }
+    it { should validate_uniqueness_of(:serial).scoped_to(:store_id) }
   end
 
   describe 'associations' do
