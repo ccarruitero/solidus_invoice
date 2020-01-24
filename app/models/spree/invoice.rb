89872c2823e0448ba4e23ff7addc3c89b0cc0531
César Carruitero <ccarruitero@protonmail.com>
2020-01-24 17:13:46 -0500

    add utility methods for generate sunat invoice

--- app/models/spree/invoice.rb
+++ app/models/spree/invoice.rb
@@ -8,14 +8,54 @@ class Spree::Invoice < Spree::Base
   belongs_to :order
   belongs_to :invoice_serial
 
-  delegate :doc_type, to: :invoice_serial
+  delegate :doc_type, :serial, to: :invoice_serial
 
   preference :sunat_attributes, :hash
 
   def sunat_attributes
     attrs = (preferred_sunat_attributes || {}).dup
-    provider = SunatInvoice::Provider.new(attrs[:provider])
+    provider = SunatInvoice::Provider.new(attrs[:provider] || {})
     attrs[:provider] = provider
     attrs
+  end
+
+  # return a `SunatInvoice::Invoice`
+  def build_sunat_invoice
+    # generate SunatInvoice::Invoice
+    sunat_invoice = SunatInvoice::Invoice.new(sunat_attributes)
+
+    # add invoice lines from line_items
+    order.line_items.map do |line_item|
+      add_sunat_line(sunat_invoice, line_item)
+    end
+
+    # add invoice lines from shipment
+    order.shipments.map do |shipment|
+      next unless shipment.amount.positive?
+
+      add_sunat_line(sunat_invoice, shipment)
+    end
+
+    # store xml
+    store_xml(sunat_invoice.xml) if respond_to?(:store_xml)
+
+    sunat_invoice
+  end
+
+  # add a new `SunatInvoice::Item` to the given sunat invoice
+  # sunat_invoice => `SunatInvoice::Invoice` instance to add lines
+  # resource => an order's line_item or shipment
+  def add_sunat_line(sunat_invoice, resource)
+    # TODO: handle items with taxes different than vat
+    tax_amount = resource.adjustments.tax.sum(:amount)
+    tax = SunatInvoice::Tax.new(amount: tax_amount, tax_type: :igv)
+    quantity = resource.respond_to?(:quantity) ? resource.quantity : 1
+    item_attrs = {
+      quantity: quantity,
+      price: resource.price_excluding_vat,
+      price_code: '01',
+      taxes: [tax]
+    }
+    sunat_invoice.lines << SunatInvoice::Item.new(item_attrs)
   end
 end
--- spec/models/spree/invoice_spec.rb
+++ spec/models/spree/invoice_spec.rb
@@ -11,4 +11,45 @@ RSpec.describe Spree::Invoice, type: :model do
     it { should belong_to(:order) }
     it { should belong_to(:invoice_serial) }
   end
+
+  context 'sunat invoice' do
+    let(:invoice) { create(:invoice_with_serial) }
+    let(:order) { create(:order_ready_to_ship, store: invoice.invoice_serial.store) }
+
+    before do
+      invoice.update(order: order)
+    end
+
+    describe '#build_sunat_invoice' do
+      subject(:sunat_invoice) { invoice.build_sunat_invoice }
+
+      it 'return a sunat invoice instance' do
+        expect(sunat_invoice.class).to eq(SunatInvoice::Invoice)
+      end
+
+      it 'get same total than order' do
+        sunat_invoice.prepare_totals
+        expect(invoice.order.total).to eq(sunat_invoice.total.to_d)
+      end
+    end
+
+    describe '#add_sunat_line' do
+      let(:sunat_invoice) { SunatInvoice::Invoice.new(invoice.sunat_attributes) }
+      let(:line) { order.line_items.first }
+
+      before do
+        invoice.add_sunat_line(sunat_invoice, line)
+      end
+
+      it 'add line to sunat invoice' do
+        expect(sunat_invoice.lines.count).to eq(1)
+      end
+
+      it 'has correct values' do
+        sunat_line = sunat_invoice.lines.first
+        expect(sunat_line.quantity).to eq(line.quantity)
+        expect(sunat_line.price).to eq(line.price_excluding_vat)
+      end
+    end
+  end
 end
